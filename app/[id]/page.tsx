50c15b20d911342314430567a3ecabbd528c81c6
stefanluth <stefan.luthardt@outlook.com>
2024-06-23 16:04:00 +0200

    Generate metadata dynamically

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,16 @@
+import { Metadata } from 'next';
+
 import CountdownCard from '@/app/_components/Countdown';
 import { prisma } from '@/app/prisma';
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const countdown = await prisma.countdown.findFirst({ where: { id: params.id } });
+
+  return {
+    title: countdown?.name ? `${countdown.name} - Countdown` : 'Until',
+    description: countdown ? `How long until ${countdown.name}?` : undefined,
+  };
+}
 
 export default async function CountdownPage({ params }: { params: { id: string } }) {
   const countdown = await prisma.countdown.findFirst({ where: { id: params.id } });
